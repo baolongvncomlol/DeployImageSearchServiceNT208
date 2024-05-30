c1dcee8fb0778e7bac5c1d02d23c73dbe1b7fda5
baolongvncom <baolongvncom@gmail.com>
2024-05-30 19:37:05 +0700

    first commit

---
+++ offline.py
@@ -0,0 +1,66 @@
+from PIL import Image
+from feature_extractor import FeatureExtractor
+from pathlib import Path
+import numpy as np
+import os
+from pymongo import MongoClient
+import requests
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://baolongvncom:baolong123456@cluster0.0vgsjr7.mongodb.net")
+db = client["WatchShop"]
+collection = db["watches"]
+
+watches = collection.find()
+
+    
+def train():
+    fe = FeatureExtractor()
+
+    for watch in watches:
+        try:
+            img_path = watch['image']
+            print(img_path)  # e.g., ./static/img/xxx.png
+            feature = fe.extract(img=Image.open(requests.get("https://deploynt208backend.onrender.com" + img_path, stream=True).raw))
+
+            # Tạo đường dẫn đến thư mục static/feature
+            feature_dir = Path("static/feature")
+
+            img_path = img_path.split('/')[-1]
+
+            img_path = img_path.split('.')[0]
+
+            # Tạo đường dẫn cho file feature
+            feature_path = feature_dir / (img_path + ".npy")
+
+            print(feature_path)
+
+            # Tạo thư mục nếu nó chưa tồn tại
+            feature_dir.mkdir(parents=True, exist_ok=True)
+
+            # Ghi đè feature lên file nếu nó đã tồn tại, hoặc tạo mới nếu chưa có
+            np.save(feature_path, feature)
+        except:
+            continue
+
+def train_image(image_filename):
+
+    print(image_filename)
+
+    image_path = "/images/" + image_filename
+
+    try:
+        fe = FeatureExtractor()
+        feature = fe.extract(img=Image.open(requests.get("https://deploynt208backend.onrender.com" + image_path, stream=True).raw))
+        feature_dir = Path("static/feature")
+        feature_path = feature_dir / (image_filename.split('.')[0] + ".npy")
+        feature_dir.mkdir(parents=True, exist_ok=True)
+        np.save(feature_path, feature)
+        print("Train_image success")
+        return {"status": "success", "feature_path": feature_path}
+    
+    except:
+        print("Train_image fail")
+        return {"status": "fail"}
+if __name__ == "__main__":
+    train()
---
+++ server.py
@@ -0,0 +1,122 @@
+import numpy as np
+from PIL import Image
+import requests
+from feature_extractor import FeatureExtractor
+from datetime import datetime
+from flask import Flask, json, request, render_template, jsonify
+from pathlib import Path
+from pymongo import MongoClient
+from offline import train_image
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://baolongvncom:baolong123456@cluster0.0vgsjr7.mongodb.net")
+db = client["WatchShop"]
+collection = db["watches"]
+
+# Read image features
+fe = FeatureExtractor()
+features = []
+img_ids = []
+for feature_path in Path("static/feature").glob("*.npy"):
+    features.append(np.load(feature_path))
+    img_id = "https://deploynt208backend.onrender.com/images/" + feature_path.stem + ".png"
+
+    watch = collection.find_one({"image": img_id})
+
+    img_ids.append(watch)
+
+print(len(img_ids))
+print(len(features))
+
+
+@app.post("/retrain")
+def retrain():
+    global features
+    global img_ids
+
+    image_filename = request.form["image_filename"]
+
+    print(image_filename)
+    train_result = train_image(image_filename)
+    if train_result["status"] == "success":
+        feature_path = train_result["feature_path"]
+
+        img_id = "https://deploynt208backend.onrender.com/images/" + image_filename
+
+        print(img_id)
+
+        watch = collection.find_one({"image": img_id})
+        img_ids.append(watch)
+
+        print(feature_path)
+
+        features.append(np.load(feature_path))
+
+        print("Retrain success")
+        return {"status": "retrain success"}
+    else:
+        print("Train fail")
+        return {"status": "retrain fail"}
+
+
+@app.post("/imagesearch")
+def index():
+    global features
+    global img_ids
+
+    copy_features = features.copy()
+
+    print(request.form)
+    image_filename = request.form["query_img"]
+    image_url = "https://deploynt208backend.onrender.com/imagesearchstorage/" + image_filename
+
+    # Fetch image from URL
+    img = Image.open(requests.get(image_url, stream=True).raw)
+    img = img.convert("RGB")
+    img = img.resize((224, 224))
+
+    # Save query image
+    uploaded_img_path = (
+        "static/uploaded/"
+        + datetime.now().isoformat().replace(":", ".")
+        + "_"
+        + image_filename
+    )
+    img.save(uploaded_img_path)
+
+    # Run search
+    query = fe.extract(img)
+
+    # L2 distances to features
+    dists = []
+    # dists = np.linalg.norm(features - query, axis=(1, 2))
+    for feature in copy_features:
+        dists.append(np.linalg.norm(feature - query))
+    dists = np.array(dists)
+    print(dists)
+
+    ids = np.argsort(dists)[:8]  # Top 30 results
+
+    print(ids)
+
+    scores = []
+
+    for id in ids:
+        try:
+            print(id)
+            print(img_ids[id])
+            print(img_ids[id]["id"])
+            scores.append(img_ids[id]["id"])
+        except:
+            print("Error", id)
+            continue
+
+    return {"scores": scores}
+
+
+if __name__ == "__main__":
+    app.run(debug=False, host='0.0.0.0')
